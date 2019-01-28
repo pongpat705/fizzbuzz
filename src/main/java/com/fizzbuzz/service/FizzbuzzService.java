a98ab1c9eb390c2af907839805c911e0a1d163bf
fhluky <fhluky@gmail.com>
2019-01-28 12:40:59 +0700

    fword

--- src/main/java/com/fizzbuzz/service/FizzbuzzService.java
+++ src/main/java/com/fizzbuzz/service/FizzbuzzService.java
@@ -5,17 +5,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class FizzbuzzService {
 
-	public String inputNumber(int i) {
-		// TODO Auto-generated method stub
-		if( i%3 == 0) {
-			return "fizz";
-		}else {
-			return String.valueOf(i);
-		}
-//		return String.valueOf(2);
-	}
 
-	
+	public String inputNumber(int number) {
+		
+		String result = "";
+		
+		if(3 == number) {
+			result = "buzz";
+		} else {
+			result = String.valueOf(number);
+		}
+		
+		return result;
+	}
 
 	
 	
--- src/test/java/com/fizzbuzz/FizzbuzzApplicationTests.java
+++ src/test/java/com/fizzbuzz/FizzbuzzApplicationTests.java
@@ -8,8 +8,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import com.fizzbuzz.service.FizzbuzzService;
 
-import ch.qos.logback.core.net.SyslogOutputStream;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class FizzbuzzApplicationTests {
@@ -37,28 +35,21 @@ public class FizzbuzzApplicationTests {
 		}else {
 			System.out.println("false");
 		}
+		
 	}
 	
 	@Test
-	public void inputThreeShouldbeOutputFizz() {
+	public void inputThreeShouldbeOutputBuzz() {
 		String result = fizzbuzzService.inputNumber(3);
-		if(result.equals("fizz")) {
+		if(result.equals("buzz")) {
 			System.out.println("pass");
 		}else {
 			System.out.println("false");
 		}
+		
 	}
 	
-	@Test
-	public void inputFourShouldbeOutputFour() {
-		String result = fizzbuzzService.inputNumber(4);
-		if(result.equals("4")) {
-			System.out.println("pass");
-		} else {
-			System.out.println("false");
-		}
-	}
-	
+		
 
 }
 
